26caac62a5e0fc48ad2792e8d7f205e79d605639
IrinaSavosh <irina_savosh@mail.ru>
2023-08-01 15:38:53 +0300

    Refinement of the 'Weight of toys' method and modification of the 'Winning Toys' method

--- Java/App.java
+++ Java/App.java
@@ -1,3 +1,25 @@
+// Необходимо написать программу – розыгрыша игрушек в магазине детских товаров.
+// Стараемся применять ООП и работу с файлами.
+// Если какой-то пункт не изучали и не знаете, как сделать, то можете сделать своим способом. Например, у кого в курсе не было ООП, то применяем списки\массивы\словари
+ 
+// Желательный функционал программы:
+// В программе должен быть минимум один класс со следующими свойствами:
+// id игрушки,
+// текстовое название,
+// количество
+// частота выпадения игрушки (вес в % от 100)
+ 
+// Метод добавление новых игрушек и возможность изменения веса (частоты выпадения игрушки)
+// Возможность организовать розыгрыш игрушек.
+
+// Например, следующим образом:
+// С помощью метода выбора призовой игрушки – мы получаем эту призовую игрушку и записываем в список\массив.
+// Это список призовых игрушек, которые ожидают выдачи.
+// Еще у нас должен быть метод – получения призовой игрушки.
+// После его вызова – мы удаляем из списка\массива первую игрушку и сдвигаем массив. А эту игрушку записываем в текстовый файл.
+// Не забываем уменьшить количество игрушек
+
+
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -10,11 +32,11 @@ public class App {
 
             if(!toyFile.exists()){
                 toyFile.createNewFile();
-                shop.add(new Toy(1,"Кукла Барби", 5));
-                shop.add(new Toy(2,"Мишка", 8));
-                shop.add(new Toy(3,"Машинка", 12));
-                shop.add(new Toy(4,"Конструктор Lego", 7));
-                shop.add(new Toy(5,"Динозаврик", 20));
+                shop.adding(new Toy(1,"Кукла Барби", 5));
+                shop.adding(new Toy(2,"Мишка", 8));
+                // shop.add(new Toy(3,"Машинка", 12));
+                // shop.add(new Toy(4,"Конструктор Lego", 7));
+                // shop.add(new Toy(5,"Динозаврик", 20));
                 // shop.conversionWeight(0); что-нить придумать с айди
                 shop.saveToFile("toys.csv");
                 
@@ -22,15 +44,16 @@ public class App {
                 shop.LoadFromFile("toys.csv");
             }
             // shop.setWeight(1, 30); //изменить вес куклы
-            shop.add(new Toy(1,"Кукла Барби", 5));
-            ArrayList<String> toyList = shop.getToyList(); // получить список игрушек в магазине
-            for (String toy : toyList) {
-                System.out.println(toy);
-            }
-            // ArrayList<Toy> winners = shop.playGame(3); //разыграть 3 игрушки 
-            // for (Toy t : winners) {
-                // System.out.println(t.getName());
+            // shop.adding(new Toy(1,"Кукла Барби", 5));
+            // shop.saveToFile("toys.csv");
+            // ArrayList<String> toyList = shop.getToyList(); // получить список игрушек в магазине
+            // for (String toy : toyList) {
+            //     System.out.println(toy);
             // }
+            ArrayList<Toy> winners = shop.playGame(3); //разыграть 3 игрушки 
+            for (Toy t : winners) {
+                System.out.println(t.getName());
+            }
 
             shop.saveToFile("toys.csv"); //сохранить данные об игрушках в файл
         
--- Java/ToyShop.java
+++ Java/ToyShop.java
@@ -16,23 +16,32 @@ public class ToyShop {
 
    // Метод добавления игрушек
 
-   public void add(Toy toy) {
+   public void adding(Toy toy) {
       boolean foundToy = false;
       for (Toy t : toys) {
          if (t.getID() == toy.getID()) {
+            totalNumber += toy.getQuantity(); // мой
             t.setQuantity(t.getQuantity() + toy.getQuantity());
-            totalNumber += t.getQuantity(); //мой
-            System.out.println(totalNumber);
-            t.setWeight(conversionWeight(t.getID())); //мой
+
+            // System.out.println(totalNumber);
+            // t.setWeight(conversionWeight(t.getID())); // мой
+            // System.out.println("ID: " + t.getID() + "... Вес: " + t.getWeight());
             foundToy = true;
             break;
          }
-         
+
       }
       if (!foundToy) {
          toys.add(toy);
-         totalNumber += toy.getQuantity(); //мой
-         toy.setWeight(conversionWeight(toy.getID())); //мой
+         totalNumber += toy.getQuantity(); // мой
+         System.out.println(totalNumber);
+
+      }
+      for (Toy t : toys) {
+         t.setWeight(conversionWeight(t.getID())); // мой
+         System.out.println(
+               "Общее количество - " + totalNumber + ". Количество этой игрушки - " + t.getQuantity() + ". ID -  "
+                     + t.getID() + ". Вес -  " + t.getWeight());
       }
    }
 
@@ -41,16 +50,16 @@ public class ToyShop {
    public double conversionWeight(int toyID) {
       double tt = 0;
       for (Toy t : toys) {
-         if(t.getID() == toyID){
-         // t.setWeight(weight);
-         t.setWeight(t.getQuantity()/totalNumber*100); //мой
-         tt = t.getWeight();
-         break;
-         //String.format("%.3f",value); округление с переводом в строку
-         // Math.round(number * count) / count;
+         if (t.getID() == toyID) {
+            // t.setWeight(weight);
+            t.setWeight(t.getQuantity() / totalNumber * 100); // мой
+            tt = t.getWeight();
+            break;
+            // String.format("%.3f",value); округление с переводом в строку
+            // Math.round(number * count) / count;
          }
       }
-      
+
       return tt;
    }
 
@@ -67,50 +76,81 @@ public class ToyShop {
    }
 
    // Метод розыгрыша игрушек
-   public ArrayList<Toy> playGame(int count){
-      ArrayList<Toy> winners = new ArrayList<>();
-      double weightSum = 0;
-      for (Toy t : toys) {
-         weightSum += t.getWeight();
+   // Этот метод можно преобразовать с ориентацией не на удельный вес, а на цену
+   // игрушки.
+   // Метод должен будет запонять массив сначала дешевыми игрушкми. По мере
+   // уменьшения более дешевых игрушек, в список лотерейных будут попадать игрушки
+   // с более высокой ценой. Но это уже
+   // другая история игрушек
+   public ArrayList<Toy> playGame(int count) {// возвращаемый масив метод, на входе кот число розыгрышей
+      ArrayList<Toy> winners = new ArrayList<>();// массив с типом Тоу
+      ArrayList<Toy> raffleToys = new ArrayList<>();// массив с типом Тоу
+      if(count>toys.size()){
+         count = toys.size();
       }
-
-      Random random = new Random();
-      for (int i = 0; i < count; i++) {
-         double randomNumber = random.nextDouble() * weightSum;
-         double currentSum = 0;
+      while (raffleToys.size() < count) {
+         int raffleCount;
          for (Toy t : toys) {
-            currentSum += t.getWeight();
-            if (currentSum >= randomNumber){
-               if(t.getQuantity()> 0){
-                  winners.add(t);
-                  t.setQuantity(t.getQuantity()-1);
-                  weightSum -= t.getWeight();
-               }
-               break;
+            raffleCount = (int) Math.round(t.getQuantity() / totalNumber * t.getQuantity());
+            for (int i = 0; i < raffleCount; i++) {
+               raffleToys.add(t);
+               t.setQuantity(t.getQuantity() - 1);
             }
          }
+         // double weightSum = 0; // сумма весов, т.е. всех %%
+         // for (Toy t : toys) { // перебор общего (главноного/верхнего) массива
+         // weightSum += t.getWeight(); // сложение всех %весов
+         // System.out.println(weightSum);
+
+         // }
       }
+      Random random = new Random();// создание рандома
+      int i = 0;
+      while (i < count) {// перебор до количества розыгрышей
+         if (raffleToys.size() < 1) {
+            break;
+         }
+         int randomNumber = random.nextInt(raffleToys.size()); //
+         // double currentSum = 0;// текущая сумма
+         // for (Toy t : toys) {// перебор значений верхнего массива
+         // currentSum += t.getWeight();
+         // if (currentSum >= randomNumber) {
+         // if (t.getQuantity() > 0) {
+         winners.add(raffleToys.get(randomNumber));
+         raffleToys.remove(randomNumber);
+         // t.setQuantity(t.getQuantity() - 1);
+         // weightSum -= t.getWeight();
+         // t.setWeight(conversionWeight(t.getID()));
+         // i++;
+
+         // break;
+      }
+
       return winners;
    }
 
-   //Метод сохранения данных об игрушках в файл
-   public void saveToFile(String fileName) throws IOException{
-      try (FileWriter writer = new FileWriter(new File(fileName))){
+   // Метод сохранения данных об игрушках в файл
+   public void saveToFile(String fileName) throws IOException {
+      try (FileWriter writer = new FileWriter(new File(fileName))) {
          for (Toy t : toys) {
-            writer.write(t.getID() + "," + t.getName() + "," + t.getQuantity());
+            writer.write(t.getID() + "," + t.getName() + "," + t.getQuantity() + "," + t.getWeight());
             writer.write("\n");
          }
       }
    }
 
-   //Метод загрузки данных об игрушках из файла
-   public void LoadFromFile(String fileName) throws IOException{
-      try (BufferedReader reader = new BufferedReader(new FileReader(new  File(fileName)))){
+   // Метод загрузки данных об игрушках из файла
+   public void LoadFromFile(String fileName) throws IOException {
+      try (BufferedReader reader = new BufferedReader(new FileReader(new File(fileName)))) {
          String line;
-         while ((line = reader.readLine()) != null){
+         while ((line = reader.readLine()) != null) {
             String[] toyData = line.split(",");
             Toy toy = new Toy(Integer.parseInt(toyData[0]), toyData[1], Integer.parseInt(toyData[2]));
             toys.add(toy);
+            totalNumber += toy.getQuantity(); // мой
+            // System.out.println("ЧТЕНИЕ-----Общее количество - " + totalNumber + ".
+            // Количество этой игрушки - " + toy.getQuantity() + ". ID - "
+            // + toy.getID() + ". Вес - " + toy.getWeight());
          }
       }
    }
